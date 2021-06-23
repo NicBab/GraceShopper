b6b07600f3f213bb14e4d25725e73abda5c81c7b
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-06-22 23:51:33 -0500

    added scripts and proxy setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <>
+         
+        </>
+    )
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from './components';
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+    <Router>
+        <React.StrictMode>
+            <App />, document.getElementById('root')
+        </React.StrictMode>
+    </Router>
+    
 )
