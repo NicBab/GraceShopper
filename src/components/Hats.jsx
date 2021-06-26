45693048315abe6adb2fe96a562c3ea5c0b63b0a
Michelle <mchllgndrn@gmail.com>
2021-06-25 19:24:45 -0500

    MG/chore: Moved products state to app

--- src/components/Hats.jsx
+++ src/components/Hats.jsx
@@ -1,19 +1,13 @@
-import React, { useEffect, useState} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { getAllProducts } from '../api'
 import { Card, Button } from 'react-bootstrap'
 import './Hats.css';
 
 const Hats= () => {
-    const [product, setProduct] = useState([])
-
-    useEffect(() => {
-        getAllProducts()
-    }, [])
 
     return (
         <>
-        <div className="hats">Hats</div>
+        {/* <div className="hats">Hats</div>
           <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src="holder.js/100px180"/>
              <Card.Body>
@@ -23,7 +17,7 @@ const Hats= () => {
                 <Card.Text>{product.price}Price</Card.Text>
               <Link to="/MyCart"><Button variant="primary">Add to Cart</Button></Link>
             </Card.Body>
-          </Card>
+          </Card> */}
         </>
     )
 }
