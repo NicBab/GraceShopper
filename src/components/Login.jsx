8fdf00accaf6b6ab6e8de671d4f7d59bcca59abb
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-06-29 21:34:03 -0500

    revamp login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,65 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap'
-import './css/Login.css'
+import React, { useState, useRef, } from 'react'
+import { Link } from 'react-router-dom'
+import { Container, Card, Form, Button, Alert } from 'react-bootstrap'
 
 const Login = () => {
+    const [loading, setLoading] = useState()
+    const [error, setError] = useState()
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            
+        } catch (error) {
+            console.error("Failed to sign in!")
+        }
+    }
+
+
     return (
-        <>
-        <div className="log">Login</div>
-        <Form style={{width: "15em"}}>
-        <Form.Group controlId="formBasicPassword">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+      <>
+        <Container 
+          className="d-flex align-items-center justify-content-center" 
+          style={{ minHeight: "100vh"}}>
+            <div className="w-100" style={{maxWidth: "400px"}}>
+                <Card>
+                    <Card.Body>
+                        <h2 className="text-center mb-4">Log In</h2>
+                            {error && <Alert variant="danger">{error}</Alert>}
+                            <Form onSubmit={handleSubmit}>
+                            <Form.Group id="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" ref={emailRef} required />
+                            </Form.Group>
+                            <Form.Group id="password">
+                                <Form.Label>password</Form.Label>
+                                <Form.Control type="password" ref={passwordRef} required />
+                            </Form.Group>
+                            <Button 
+                                disabled={loading} 
+                                className="w-100" 
+                                type="submit">Login
+                            </Button>
+                            </Form>
+                        <div className="w-100 text-center mt-3">
+                            <Link to="/forgot-password">Forgot Password?</Link>
+                        </div>
+                    </Card.Body>
+                    </Card>
+                <div className="w-100 text-center mt-2">
+                    Need an account? <Link to="/signup">Sign Up here</Link> 
+                </div>
+                <div className="w-100 text-center mt-2">
+                    <Link to="/home">Return to home page</Link>
+                </div>
+            </div>
+         </Container>
       </>
     )
 }
 
-export default Login;
+export default Login 
